109b26110ac1afbc34e77d32cce9e8d6287cacf5
Sivakumar <nagireddysivakumar952@gmail.com>
2025-12-14 22:08:20 +0530

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <div className='text-6xl font-bold'>Habit Tracker</div>
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="mb-8">
+      <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-2">Habit Tracker</h1>
+      <p className="text-gray-600">Build better habits, one day at a time</p>
+    </div>
+  );
+}
